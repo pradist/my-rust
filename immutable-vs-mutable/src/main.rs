4e8ebcde9e9296080c4636b79423b23be25d84fe
Pradist Keawborisut <pradist.ke@gmail.com>
2024-12-13 19:38:11 +0700

    feat: add immutable-vs-mutable

---
+++ immutable-vs-mutable/src/main.rs
@@ -0,0 +1,7 @@
+fn main() {
+    let mut crabby_energy = 100;
+    println!("The value of x is: {}", crabby_energy);
+
+    crabby_energy = 200;
+    println!("The value of x is: {}", crabby_energy);
+}
